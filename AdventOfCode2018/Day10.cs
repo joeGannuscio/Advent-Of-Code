69313c77a59b39aa21b275a840d2358a6d78ddd7
Joe Gannuscio <gannuscio_joseph_m@cat.com>
2018-12-11 11:09:44 -0600

    day 10 WIP

---
+++ AdventOfCode2018/Day10.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    public class Day10
+    {
+        private string _inputPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "InputFiles/Day10Input.txt");
+        private List<Point> _input;
+
+        public Day10()
+        {
+            _input = InputReader(_inputPath);
+        }
+
+        public void GetResults()
+        {
+            Part1(_input);
+        }
+
+        public void Part1(List<Point> input)
+        {
+            var time = 0;
+            var userEntry = "";
+
+            while (!userEntry.Equals("stop"))
+            {
+                foreach (var point in input)
+                {
+                    point.XPos += point.XVel;
+                    point.YPos += point.YVel;
+                }
+
+                var xMin = input.Min(p => p.XPos);
+                var xMax = input.Max(p => p.XPos);
+                var yMin = input.Min(p => p.YPos);
+                var yMax = input.Max(p => p.YPos);
+
+                for (var i = yMin; i <= yMax; i++)
+                {
+                    for (var j = xMin; j <= xMax; j++)
+                    {
+                        Console.Write(input.Any(p => p.XPos == j && p.YPos == i) ? "*" : ".");
+                    }
+                    Console.WriteLine();
+                }
+
+                
+                userEntry = Console.ReadLine();
+
+            }            
+        }
+
+        private List<Point> InputReader(string inputString)
+        {
+            var stringList = new List<string>();
+            var inputList = new List<Point>();
+
+            var fileStream = File.OpenRead(inputString);
+            var streamReader = new StreamReader(fileStream);
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                stringList.Add(line);
+            }
+
+            foreach (var str in stringList)
+            {
+                var splitStr = Regex.Split(str, "[^\\d-]+");
+                inputList.Add(new Point()
+                {
+                    XPos = Convert.ToInt32(splitStr[1]),
+                    YPos = Convert.ToInt32(splitStr[2]),
+                    XVel = Convert.ToInt32(splitStr[3]),
+                    YVel = Convert.ToInt32(splitStr[4])
+                });
+            }
+
+            return inputList;
+        }
+    }
+
+    public class Point
+    {
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int XVel { get; set; }
+        public int YVel { get; set; }
+    }
+}
--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -43,6 +43,10 @@ namespace AdventOfCode2018
                     var day8 = new Day8();
                     Console.WriteLine(day8.GetResults());
                     break;
+                case "10":
+                    var day10 = new Day10();
+                    day10.GetResults();
+                    break;
                 default:
                     Console.WriteLine("Not found");
                     break;     
