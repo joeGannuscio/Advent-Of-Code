a35bb9fa51989b11606270e9c6a1dbaa463c72df
joeGannuscio <jmgann714@gmail.com>
2018-12-19 15:33:12 -0600

    completed day 12

--- AdventOfCode2018/Day12.cs
+++ AdventOfCode2018/Day12.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 
 namespace AdventOfCode2018
@@ -24,27 +29,78 @@ namespace AdventOfCode2018
         public string GetResults()
         {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.Append("Day 12 Part 1 Solution: " + Part1(_initialState, _input));
-            stringBuilder.Append("\nDay 12 Part 2 Solution: " + Part2(_input));
+            stringBuilder.Append("Day 12 Part 1 Solution: " + Part1(_initialState, _input, 20));
+            stringBuilder.Append("\nDay 12 Part 2 Solution: " + Part2(_initialState,_input));
             return stringBuilder.ToString();
         }
 
-        public string Part1(string initialState, List<string> inputList)
+        public string Part1(string initialState, List<string> inputList, int numberOfGenerations)
         {
             var rules = GetRulesDictionary(inputList);
-            
-            //20 generations
-            for (var i = 0; i < 20; i++)
+            var currentState = initialState;
+            var genBuilder = new StringBuilder();
+            var sum = 0;
+            var addedToFront = 0;
+
+            // 20 generations
+            for (var i = 0; i < numberOfGenerations; i++)
             {
-                //expand and loop through plants
+                currentState = "..." + currentState + "...";
+                genBuilder.Clear();
+
+                addedToFront += 1;
+
+                for (var p = 2; p < currentState.Length - 2; p++)
+                {
+                    var window = currentState.Substring(p - 2, 5);
+
+                    if (rules.ContainsKey(window))
+                    {
+                        genBuilder.Append(rules[window]);
+                    }
+                    else
+                    {
+                        genBuilder.Append(".");
+                    }
+                }
+
+                currentState = genBuilder.ToString();
+            }
+
+            for (var p = 0; p < currentState.Length; p++)
+            {
+                if (currentState[p].Equals('#'))
+                {
+                    sum += (p - addedToFront);
+                }
+            }
+
+            return sum.ToString();
+        }
+
+        public string Part2(string initialState, List<string> inputList)
+        {
+            //stabilizes at 23 new plants per iteration
+            //find the point where it stabilizes and then extrapolate to 50 billion
+
+            var curSum = 0;
+            var prevSum = 0;
+            long result = 0;
+
+            for (var i = 0; i < 1000; i++)
+            {
+                prevSum = curSum;
+                curSum = Convert.ToInt32(Part1(initialState, inputList, i));
+
+                if (curSum - prevSum == 23)
+                {
+                    result = curSum + 23 * (50000000000 - i);
+                    return result.ToString();
+                }
             }
 
             return "";
-        }
 
-        public string Part2(List<string> inputList)
-        {
-            return "";
         }
 
         private List<string> InputReader(string inputString)
--- AdventOfCode2018Tests/Day12Tests.cs
+++ AdventOfCode2018Tests/Day12Tests.cs
@@ -27,10 +27,10 @@ namespace AdventOfCode2018Tests
                 "###.# => #",
                 "####. => #",
             };
-
+            var generations = 20;
             var expected = "325";
             var day12 = new Day12();
-            var result = day12.Part1(initialState, sampleInput);
+            var result = day12.Part1(initialState, sampleInput, generations);
 
             Assert.Equal(expected, result);
         }
