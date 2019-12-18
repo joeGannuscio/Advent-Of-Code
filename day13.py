65a8fbef1544eda53a993c6bbd86f14d1ce54f4d
Joe Gannuscio <jmgann714@gmail.com>
2019-12-18 14:42:03 -0600

    finally finished day 13 part 2

--- day13.py
+++ day13.py
@@ -1,4 +1,5 @@
 from intcode import Intcode
+from collections import defaultdict
 
 def main():
     input = readInput('Inputs/day13.txt')
@@ -7,7 +8,7 @@ def main():
 
 def part1(input):
     intcodeRunner = Intcode(input)
-    tiles = {}
+    tiles = defaultdict(tuple)
     count = 0
 
     while not intcodeRunner.done:
@@ -30,8 +31,8 @@ def display(tiles):
     minY = min(y for _,y in tiles)
     maxY = max(y for _,y in tiles)
 
-    height = maxY - minY + 1
-    width = maxX - minX + 1
+    height = maxY - minY
+    width = maxX - minX
 
     grid = [[' '] * width for _ in range(height)]
 
@@ -47,6 +48,7 @@ def display(tiles):
 
 def part2(input):
     intcodeRunner = Intcode(input)
+    intcodeRunner.memory[0] = 2
     tiles = {}
     
     score = 0
@@ -55,11 +57,26 @@ def part2(input):
     while not intcodeRunner.done:
         intcodeRunner.run()
 
+        inp = 0
+
         for i in range(0, len(intcodeRunner.outputs), 3):
             x, y, tile = intcodeRunner.outputs[i:i+3]
             tiles[(x,y)] = tile
 
+            if (x, y) == (-1, 0):
+                score = tile
+
+            if tile == 3:
+                paddle = x
+            elif tile == 4:
+                if x > paddle:
+                    inp = 1
+                elif x < paddle:
+                    inp = -1
+
         print(display(tiles))
+        intcodeRunner.inputs.append(inp)
+        intcodeRunner.outputs.clear()
 
     print(score)
 
