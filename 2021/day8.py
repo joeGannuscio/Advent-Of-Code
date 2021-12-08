065e8c3b6c17960ad3056b0f8e3830ab67ca0152
Joe <jmgann714@gmail.com>
2021-12-08 01:00:24 -0600

    2021 day 8 complete

--- 2021/day8.py
+++ 2021/day8.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+
+
 def part1(input_file):
     input = readInputFile(input_file)
     outputs = []
@@ -9,14 +12,55 @@ def part1(input_file):
         for v in o:
             if len(v) == 2 or len(v) == 3 or len(v) == 4 or len(v) == 7:
                 total += 1
-    
+
     print(total)
     return
 
+
 def part2(input_file):
     input = readInputFile(input_file)
+    signal_patterns = []
+    outputs = []
+
+    # make a tempalte for each digit
+    # map the template value to a permutation of the letters
+    # apply the mapped value to the signal pattern
+    # check each possibility against the template until you get a match
+    # figure out the outputs digits from the now known digits
+
+    templates = {'abcefg': '0', 'cf': '1', 'acdeg': '2', 'acdfg': '3', 'bcdf': '4', 'abdfg':'5', 'abdefg': '6', 'acf': '7', 'abcdefg': '8', 'abcdfg': '9'}
+
+    sum = 0
+    for line in input:
+        signal_patterns = line[0].split()
+        outputs = line[1].split()
+
+        for p in permutations(list('abcdefg')):
+            mapper = get_mapper(p)
+
+            matches = []
+            for pat in signal_patterns:
+                mapped_digit = ''.join(sorted(mapper[x] for x in pat))
+
+                if mapped_digit in templates.keys():
+                    matches.append(mapped_digit)
+
+            if len(matches) == 10:
+                output_digits = ''
+                for out in outputs:
+                    mapped_out = ''.join(sorted(mapper[x] for x in out))
+                    output_digits += templates[mapped_out]
+                sum += int(output_digits)
+                break
+    print(sum)
     return
 
+
+def get_mapper(perm):
+    mapper = {}
+    for templ_val, pat_val in zip('abcdefg', perm):
+        mapper[templ_val] = pat_val
+    return mapper
 
 
 def readInputFile(path):
