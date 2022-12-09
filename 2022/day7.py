6b7e641313f8801277239c0ed1c69f28029a400c
Joe <jmgann714@gmail.com>
2022-12-09 06:39:13 -0600

    finished day 7

--- 2022/day7.py
+++ 2022/day7.py
@@ -4,33 +4,61 @@ def part1(input_path):
     cwd = root
 
     build_file_system(cwd, input)
-    root.get_size()
+
+    sizes = []
+
+    def get_directory_sizes(directory):
+        sizes.append(directory.get_size())
+        for sd in directory.subdirectories:
+            get_directory_sizes(sd)
+
+    get_directory_sizes(root)
+
+    print(sum([s for s in sizes if s <= 100000]))
 
     return
 
 
 def part2(input_path):
     input = read_input_file(input_path)
+    root = Directory(name='root')
+    cwd = root
 
+    build_file_system(cwd, input)
 
-    return
+    total_space = 70000000
+    required_space = 30000000
+    used_space = root.get_size()
+
+    delete_size = required_space - (total_space-used_space)
+
+    sizes = []
+
+    def get_directory_sizes(directory):
+        sizes.append(directory.get_size())
+        for sd in directory.subdirectories:
+            get_directory_sizes(sd)
+
+    get_directory_sizes(root)
+    sizes = sorted(sizes)
+    for s in sizes:
+        if s >= delete_size:
+            print(s)
+            return
 
 
 def build_file_system(cwd, instructions):
     for instruction in instructions[2:]:
         if 'dir' in instruction:
             name = instruction.split()[1]
-            if name not in cwd.subdirectories:
-                cwd.subdirectories.append(Directory(name=name, parent=cwd))
-                continue
-
-        if '$' in instruction:
+            cwd.add_subdirectory(name)
+        elif '$' in instruction:
             if 'cd ..' in instruction:
                 cwd = cwd.parent
             elif 'cd' in instruction:
                 cwd = cwd.get_subdirectory(instruction.split()[2])
 
-        if instruction.split()[0].isdigit():
+        elif instruction.split()[0].isdigit():
             cwd.files.append(File(name=instruction.split()[1], size=instruction.split()[0]))
 
 
@@ -52,7 +80,6 @@ class Directory:
     def __init__(self, name, parent=None):
         self.name = name
         self.parent = parent
-        self.size = 0
         self.subdirectories = []
         self.files = []
 
@@ -60,15 +87,18 @@ class Directory:
         return f'Directory {self.name} has {len(self.subdirectories)} subdirectories and {len(self.files)} files'
 
     def get_size(self):
-        print(self.name)
-        self.size += self._get_total_file_size()
-        print(self.size)
-
+        size = 0
+        size += self._get_total_file_size()
         for sd in self.subdirectories:
-            sd.get_size()
-            self.size += sd.size
+            size += sd.get_size()
+        return size
 
-        return
+    def add_subdirectory(self, name):
+        if name not in self.get_subdirectory_names():
+            self.subdirectories.append(Directory(name, self))
+
+    def get_subdirectory_names(self):
+        return [n.name for n in self.subdirectories]
 
     def _get_total_file_size(self):
         size = 0
@@ -77,7 +107,10 @@ class Directory:
         return size
 
     def get_subdirectory(self, subdirectory_name):
-        return next(sd for sd in self.subdirectories if sd.name == subdirectory_name)
+        for sd in self.subdirectories:
+            if sd.name == subdirectory_name:
+                return sd
+        return None
 
 
 if __name__ == '__main__':
