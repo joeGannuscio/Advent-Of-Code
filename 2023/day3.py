7129494dcf2f070328a8b0830661a5f412debc70
Joe <jmgann714@gmail.com>
2023-12-03 16:45:22 -0600

    completed day3 part 2

--- 2023/day3.py
+++ 2023/day3.py
@@ -47,15 +47,69 @@ def check_neighbors(grid, y, x):
     return False
 
 
-
-        
-
-
-
 def part2(input_path):
     input = read_input_file(input_path)
 
+    gear_ratios = []
+
+    for y in range(len(input)):
+        for x in range(len(input[0])):
+            if input[y][x] == '*':
+                ratio = check_gears(input, y, x)
+
+                if ratio:
+                    gear_ratios.append(ratio)
+
+    print(sum(gear_ratios))
     return
+
+
+def check_gears(grid, y, x):
+    moves = [(0,1), (0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,-1),(-1,1)]
+    max_x = len(grid[0])
+    max_y = len(grid)
+
+    gears = []
+
+    for move in moves:
+        neighbor = (y + move[0], x + move[1])
+        if 0 <= neighbor[0] < max_y and 0 <= neighbor[1] < max_x:
+            val = grid[neighbor[0]][neighbor[1]]
+
+            if val.isdigit():
+                gear = get_whole_number(grid[neighbor[0]], neighbor[1])
+                if gear not in gears: # probably an unfair assumption, but it worked
+                    gears.append(gear)
+
+    if len(gears) == 2:
+        return int(gears[0])*int(gears[1])
+
+    return   
+
+def get_whole_number(row, x):
+    
+    number = [row[x]]
+
+    # check right
+    i = x+1
+
+    while row[i].isdigit():
+        number.append(row[i])
+        i += 1
+
+        if i == len(row):
+            break
+            
+
+    # check left
+    j = x-1
+    while row[j].isdigit():
+        number.insert(0, row[j])
+        j -= 1
+
+    return ''.join(number)
+
+
 
 
 def read_input_file(path):
@@ -74,5 +128,5 @@ def read_input_file(path):
 
 
 if __name__ == '__main__':
-    part1('Inputs/day3.txt')
+    #part1('Inputs/day3.txt')
     part2('Inputs/day3.txt')
