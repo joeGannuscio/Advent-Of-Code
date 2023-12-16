d857e44575179239beb5f3d13df4e437ef9eb9b6
Joe <jmgann714@gmail.com>
2023-12-16 09:25:57 -0600

    accidentally used day 13 input file for day 15 input

--- 2023/day15.py
+++ 2023/day15.py
@@ -59,5 +59,5 @@ def read_input_file(path):
 
 
 if __name__ == '__main__':
-    part1('Inputs/day13.txt')
-    part2('Inputs/day13.txt')
+    part1('Inputs/day15.txt')
+    part2('Inputs/day15.txt')
